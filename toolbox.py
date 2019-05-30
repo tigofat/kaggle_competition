4a3c65b7b050aef2db135d827c0a60f247f3d8dc
tigofat <aldenold10@gmail.com>
2019-05-30 13:05:57 +0400

    Train featurized data from other models.

--- toolbox.py
+++ toolbox.py
@@ -3,8 +3,66 @@ import pandas as pd
 
 import operator
 import ast
+from collections import Counter
 
 from sklearn.metrics import mean_squared_log_error
+
+
+def loss(predictions, targets):
+    sum_ = np.sum(np.square(predictions - targets))
+    return (sum_ / len(targets)) ** 0.5
+
+
+def root_mean_squared_log_error(predictions, targets):
+    
+    """Calculate root mean squared logarithmic error.
+    
+    Parameters
+    
+    predictions : predicted target values.
+    targets : actual target values.
+    
+    Returns
+    
+    loss : the loss implemeted with root mean squared logarithmic error.
+    sk_loss : the same loss implemeted with sklearn package.
+    """
+    
+    sum_ = np.sum(np.square(np.log(predictions + 1) - np.log(targets + 1)))
+    loss = np.sqrt(sum_ / len(targets))
+    sk_loss = np.sqrt(mean_squared_log_error(targets, predictions))
+    return loss, sk_loss
+
+
+def proc(x):
+    try: r = ast.literal_eval(x)
+    except: r =[]
+    return r
+
+
+def return_names(x):
+        r = []
+        for x_ in x:
+            r.append(x_['name'])
+        return r
+
+    
+def get_most_freq(n, ll):
+    l = []
+    for datum in ll:
+        l.extend(datum)
+    c = Counter(l)
+    return [s[0] for s in c.most_common(n)]
+
+
+def sum_freqs(ll):
+    l = []
+    for datum in ll:
+        l.extend(datum)
+    c = Counter(l)
+    def sum_freqs_ll(x):
+        return sum([c[i] for i in x])
+    return sum_freqs_ll
 
 
 def cross_validation(estimator, X, y, folds=5, **kwargs):
@@ -43,40 +101,6 @@ def save_as_csv(id, revenue, file_name='submissions', **kwargs):
     """
     
     pd.DataFrame({'revenue': revenue}, index=id).to_csv(f'{file_name}.csv', **kwargs)
-
-
-def root_mean_squared_log_error(predictions, targets):
-    
-    """Calculate root mean squared logarithmic error.
-    
-    Parameters
-    
-    predictions : predicted target values.
-    targets : actual target values.
-    
-    Returns
-    
-    loss : the loss implemeted with root mean squared logarithmic error.
-    sk_loss : the same loss implemeted with sklearn package.
-    """
-    
-    sum_ = np.sum(np.square(np.log(predictions + 1) - np.log(targets + 1)))
-    loss = np.sqrt(sum_ / len(targets))
-    sk_loss = np.sqrt(mean_squared_log_error(targets, predictions))
-    return loss, sk_loss
-
-
-def get_features(dataframe, col_name, based_on, func, **kwargs):
-    
-    # this function is helpful whenever you need, something like: 
-    # What is the maximum amount of money spent on films where played actor X.
-    
-    features = dataframe[col_name].unique()
-    
-    feature_mask = [(dataframe[col_name] == feature, feature) for feature in features]
-    means = [(feature, func(dataframe[based_on][mask], **kwargs)) for mask, feature in feature_mask]
-
-    return sorted(means, key=lambda x: x[1], reverse=True)
 
 
 def json_to_dict(feature_column):
@@ -121,18 +145,3 @@ def get_json_features(feature_column, key_value, estimate_with, oper=operator.ad
 	return sorted(value_estim.items(),
 					key=lambda x: x[1],
 					reverse=True)
-
-
-if __name__ == "__main__":
-    # Few examples using get_features function.
-    fake_df = pd.DataFrame({'films': ['movie1', 'movie2', 'movie3', 'movie4'], 
-                        'actors': ['actor1', 'actor1', 'actor2', 'actor2'],
-                       'revenue': [100, 200, 300, 400]})
-    
-    # Get movie names where the revenues are the highest (sorted from best to worst).
-    highest_rev_movies = get_features(fake_df, 'films', 'revenue', np.max)
-    
-    # Get the most expansive movie for every actor, who played in it.
-    # In this case actor2 played in two films (in movie3 and movie4), but the revenue of movie3 is higher, and the same happend 
-    # for actor1 and for every actor in the column as shown below.
-    exp_movie_for_every_actor = get_features(fake_df, 'actors', 'revenue', np.max)
